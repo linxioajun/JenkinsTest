2255c201654a6deac719a17bc524d2f23490f19b
linxiaojun <linxiaojun@shindata.com>
2021-11-10 16:10:05 +0800

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,3 +1,3 @@
 /*新增SQL16：03*/update cust1.companya set address = 'Japan' where id = 3;
-/*新增SQL16：08*/update cust1.companya set address = 'Japan' where id = 3;
+/*新增SQL16：09*/update cust1.companya set address = 'Japan' where id = 3;
 

6152ab30b8c6fbe7000f6920e415f76d3fc02d4e
linxiaojun <linxiaojun@shindata.com>
2021-11-10 16:03:45 +0800

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,26 +1,2 @@
-update cust1.companya set address = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' where id = 3;
-update cust1.company set addressd = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' wherer id = 3;
-update cust1.company set address = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' where id = 3
-update cust1.company set address = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' where id = 3;
-select * from cust1.company where 1=1;
-select * from cust1.cpm where id = 1;
-update cust1.company set address = 'Japan' where id > 1;
-update cust1.company set address = 'Japan' where id < 3;
-update cust1.company set address = 'Japan' where id = 3;
-update cust1.company set address = 'Japan' where id < 2;
-update cust1.company set address = 'Japan' where id = 2;
+/*新增SQL16：03*/update cust1.companya set address = 'Japan' where id = 3;
 
-
-
-
-
-
-
-
-select * from cust1.company where 1=1;
-update cust1.company set address = 'Japan' where id > 1;

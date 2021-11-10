150839d53eca9720048d7e03b6a87cd9313066f4
linxiaojun <linxiaojun@shindata.com>
2021-11-10 16:03:02 +0800

    Upload New File

---
+++ sql/test.sql
@@ -0,0 +1,26 @@
+update cust1.companya set address = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' where id = 3;
+update cust1.company set addressd = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' wherer id = 3;
+update cust1.company set address = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' where id = 3
+update cust1.company set address = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' where id = 3;
+select * from cust1.company where 1=1;
+select * from cust1.cpm where id = 1;
+update cust1.company set address = 'Japan' where id > 1;
+update cust1.company set address = 'Japan' where id < 3;
+update cust1.company set address = 'Japan' where id = 3;
+update cust1.company set address = 'Japan' where id < 2;
+update cust1.company set address = 'Japan' where id = 2;
+
+
+
+
+
+
+
+
+select * from cust1.company where 1=1;
+update cust1.company set address = 'Japan' where id > 1;

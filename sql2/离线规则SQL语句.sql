b30494ca2255bf302a73675cf8417ac4294cd7de
linxiaojun <linxiaojun@shindata.com>
2021-11-10 16:25:52 +0800

    Update 离线规则SQL语句.sql

--- sql2/离线规则SQL语句.sql
+++ sql2/离线规则SQL语句.sql
@@ -1,5 +1,5 @@
 --【审核规则】参与连接操作的表数量太多	
 select 1 from a left join b on a.id = b.name left join c on a.id = c.age left join d on a.id=d.id;
 
-select 1 from a left join b on a.id = b.name;
+/*目录为sql2*/select 1 from a left join b on a.id = b.name;
 

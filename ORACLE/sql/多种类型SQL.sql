c3ed7d86276ffb96ba8212b2758599a340f4523f
linxiaojun <linxiaojun@shindata.com>
2022-04-25 14:28:32 +0800

    lxj

---
+++ ORACLE/sql/多种类型SQL.sql
@@ -0,0 +1,7 @@
+SELECT Name FROM Persons;
+INSERT INTO Persons VALUES ('Bill');
+UPDATE Person SET Name = 'Fred' ; 
+DELETE FROM Person WHERE Name = 'Wilson' ;
+ALTER TABLE Persons ADD date;
+drop table table1;
+CREATE TABLE Persons(Id_P int);

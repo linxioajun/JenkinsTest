0d5aae57b6d04321e040b50cd8c1e4768f0d4d6e
linxiaojun <linxiaojun@shindata.com>
2022-04-25 15:26:42 +0800

    lxj

--- ORACLE/sql/多种类型SQL.sql
+++ ORACLE/sql/多种类型SQL.sql
@@ -5,4 +5,4 @@ DELETE FROM Person WHERE Name = 'Wilson' ;
 ALTER TABLE Persons ADD date;
 drop table table1;
 CREATE TABLE Persons(Id_P int);
-CREATE INDEX IDX_name_1 ON  T1 (SANME;
+CREATE INDEX IDX_name_1 ON  T1 (SANME);

0ae9e044a06490da6bded0d04b54f7c250d38720
linxiaojun <linxiaojun@shindata.com>
2022-04-28 15:44:28 +0800

    lxj

--- ORACLE/spliteSql/sql分割.sql
+++ ORACLE/spliteSql/sql分割.sql
@@ -7,7 +7,7 @@ SELECT Name FROM Persons2
 ; 
  SELECT Name FROM Persons3 ; SELECT Name FROM Persons4 ; SELECT Name FROM Persons5 ; 
 /* 注释在前端 */SELECT Name FROM Persons6; 
-SELECT Name FROM Persons7/* 注释在末端 */; 
+SELECT Name FROM Persons7/* 注释在 末端 */;
 SELECT Name FROM Persons8;
 SELECT Name /* 注释在中间 */FROM Persons9; 
 SELECT Name FROM Persons10; 

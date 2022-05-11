5750ec8c6aaaf79b6593a5eb4e4d993a251df81c
linxiaojun <linxiaojun@shindata.com>
2022-05-11 15:54:10 +0800

    lxj

--- MYSQL/sql分割.sql
+++ MYSQL/sql分割.sql
@@ -1,4 +1,5 @@
 /* 分号被引号包裹 */update cust1.audit_rule set examples = '{""sqlText"":""select * from stu where age < (select min(age) from stu2);"",""suggest"":""对实际需求 和SQL语句进行评估，评估是否可以简化SQL语句或使用连接操作代替子查询"",""indexes"":[{""start"":28,""end"":57,""desc"":""复杂的嵌套子查询会影响数据库优化器生成合理的执行计划，这是导致严重性能问题的常见原因"",""index"":""①""}]}' where rule_name = 'SQL中存在嵌套子查询';
+/* 分号被引号包裹 */update cust1.audit_rule set examples = "双引号中有分号;" where rule_name = 'SQL中存在嵌套子查询';
 SELECT Name FROM Persons1;
 SELECT Name FROM Persons2 
 ; 
@@ -23,4 +24,17 @@ SELECT Name FROM Persons17;
 SELECT Name FROM Persons18;
 #注释中有分号;
 SELECT Name FROM Persons19;
+/* 多行注释带分号
+ 多行注释带分号;
+ 多行注释带分号;
+ 多行注释带分号*/
+SELECT Name FROM Persons20;
+SELECT Name FROM/* 多行注释带分号
+                 多行注释带分号;
+                 多行注释带分号;
+                 多行注释带分号*/ Persons21;
+SELECT Name FROM Persons22/* 多行注释带分号
+                                           多行注释带分号;
+                                           多行注释带分号;
+                                           多行注释带分号*/;
 /* 没有分号 */SELECT Name FROM Persons15
--- SQLSERVER/sql分割.sql
+++ SQLSERVER/sql分割.sql
@@ -16,6 +16,7 @@ sp_who
 GO 
 
 /* 分号被引号包裹 */update cust1.audit_rule set examples = '{""sqlText"":""select * from stu where age < (select min(age) from stu2);"",""suggest"":""对实际需求 和SQL语句进行评估，评估是否可以简化SQL语句或使用连接操作代替子查询"",""indexes"":[{""start"":28,""end"":57,""desc"":""复杂的嵌套子查询会影响数据库优化器生成合理的执行计划，这是导致严重性能问题的常见原因"",""index"":""①""}]}' where rule_name = 'SQL中存在嵌套子查询';
+/* 分号被引号包裹 */update cust1.audit_rule set examples = "双引号中有分号;" where rule_name = 'SQL中存在嵌套子查询';
 /* BEGIN…END */BEGIN SELECT product_id,product_name FROM production.products WHERE list_price> 100000; IF @@ROWCOUNT=0 PRINT 'No product with price greater than 100000 found'; END;
 /* 嵌套BEGIN…END */BEGIN DECLARE @NAME VARCHAR (MAX); SELECT TOP 1 @NAME=product_name FROM production.products ORDER BY list_price DESC; IF @@ROWCOUNT<> 0 BEGIN PRINT 'The most expensive product is '+ @NAME END ELSE BEGIN PRINT 'No product found'; END; END;
 
@@ -40,6 +41,19 @@ SELECT Name FROM Persons14; -- 注释在末端
 SELECT Name FROM /* 注释中有分号; */Persons16;
 -- 注释中有分号;
 SELECT Name FROM Persons17;
+/* 多行注释带分号
+ 多行注释带分号;
+ 多行注释带分号;
+ 多行注释带分号*/
+SELECT Name FROM Persons20;
+SELECT Name FROM/* 多行注释带分号
+                 多行注释带分号;
+                 多行注释带分号;
+                 多行注释带分号*/ Persons21;
+SELECT Name FROM Persons22/* 多行注释带分号
+                                           多行注释带分号;
+                                           多行注释带分号;
+                                           多行注释带分号*/;
 /* 触发器 */CREATE TRIGGER production.trg_product_audit ON production.products AFTER
 INSERT AS BEGIN
 SET NOCOUNT ON;

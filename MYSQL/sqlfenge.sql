48d391107526c8efc7ed19904f3a3aba6520369f
linxiaojun <linxiaojun@shindata.com>
2022-05-12 11:33:32 +0800

    lxj

--- MYSQL/sqlfenge.sql
+++ MYSQL/sqlfenge.sql
@@ -1,35 +1,37 @@
 /* 分号被引号包裹 */update cust1.audit_rule set examples = '{"sqlText":"select * from stu where age < (select min(age) from stu2);","suggest":"对实际需求 和SQL语句进行评估，评估是否可以简化SQL语句或使用连接操作代替子查询","indexes":[{"start":28,"end":57,"desc":"复杂的嵌套子查询会影响数据库优化器生成合理的执行计划，这是导致严重性能问题的常见原因","index":"①"}]}' where rule_name = 'SQL中存在嵌套子查询';
-/* 分号被 引号包裹 */update cust1.audit_rule set examples = "双引号中有分号;" where rule_name = 'SQL中存在嵌套子查询';
-/* 触 发器 */CREATE TRIGGER TTT_nnn AFTER INSERT ON table1 FOR EACH ROW BEGIN UPDATE table2 SET ID = 1; END;
-/* 触 发器 */CREATE TRIGGER TRI_nnn AFTER INSERT ON table1 FOR EACH ROW BEGIN UPDATE table2 SET ID = 1; END;
+/* 分号被引号包裹 */update cust1.audit_rule set examples = "双引号中有分号;" where rule_name = 'SQL中存在嵌套子查询';
 SELECT Name FROM Persons1;
 SELECT Name FROM Persons2 
 ; 
  SELECT Name FROM Persons3 ; SELECT Name FROM Persons4 ; SELECT Name FROM Persons5 ; 
-/* 注释123在前端 */SELECT Name FROM Persons6;
-SELECT Name FROM Persons7/* 注释在 末端 */;
+/* 注释;在前端 */SELECT Name FROM Persons6;
+SELECT Name FROM Persons7/* 注释;在末端 */;
 SELECT Name FROM Persons8;
-SELECT Name /* 注释在中间 */FROM Persons9; 
+SELECT Name /* 注释在;中间 */FROM Persons9;
 SELECT Name FROM Persons10; 
 SELECT Name FROM Persons11 ;
 SELECT
     Name 
 FROM
     Persons12; 
--- 注释   为--
+-- 注释为--
 SELECT Name FROM Persons13;
 SELECT Name FROM Persons14; -- 注释在末端
 SELECT Name FROM /* 注释中有分号; */Persons16;
 -- 注释中有分号;
 SELECT Name FROM Persons17;
-/* 多行注  释带分号
+#注释为#
+SELECT Name FROM Persons18;
+#注释中有分号;
+SELECT Name FROM Persons19;
+/* 多行注释带分号
  多行注释带分号;
  多行注释带分号;
  多行注释带分号*/
 SELECT Name FROM Persons20;
 SELECT Name FROM/* 多行注释带分号
                  多行注释带分号;
-                 多行注释带分号;/
+                 多行注释带分号;
                  多行注释带分号*/ Persons21;
 SELECT Name FROM Persons22/* 多行注释带分号
                                            多行注释带分号;
